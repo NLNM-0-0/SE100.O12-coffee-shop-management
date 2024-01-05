835195d15dde2f01bb8064891e9b2cd9f453e5f8
NLNM-0-0 <nguyenlengocmai000@gmail.com>
2024-01-05 19:40:52 +0700

    dev: auth feature

---
+++ frontend/app/(auth)/login/page.tsx
@@ -0,0 +1,27 @@
+"use server"
+import styles from './login.module.css'
+import Image from 'next/image';
+import LoginForm from '@/components/login/loginForm';
+
+const Login = async () => {
+    return (
+        <div className={styles.container}>
+            <div className={styles.leftColumn}>
+                <Image src='/login-background.png' alt='' fill className={styles.image} />
+            </div>
+
+            <div className={styles.rightColumn}>
+                <div className={styles.rightHeader}>
+                    <h1 className={styles.title}>Đăng nhập vào tài khoản</h1>
+                    <h5 className={styles.description}>Xem những gì đang xảy ra với doanh nghiệp của bạn</h5>
+                </div>
+
+                <div className={styles.formContainer}>
+                    <LoginForm />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Login;
