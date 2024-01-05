9f51bf3a17821a2987379d19496f28a3e196edcb
kimthu09 <21521495@gm.uit.edu.vn>
2024-01-05 23:54:31 +0700

    dev: supplier detail ui

---
+++ frontend/lib/supplier/getSupplier.ts
@@ -0,0 +1,32 @@
+import { endPoint } from "@/constants";
+import { Supplier } from "@/types";
+import useSWR from "swr";
+import { getApiKey } from "../auth/action";
+
+const fetcher = async (url: string) => {
+  const token = await getApiKey();
+  return fetch(url, {
+    headers: {
+      accept: "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    cache: "no-store",
+  })
+    .then((res) => {
+      return res.json();
+    })
+    .then((json) => json.data);
+};
+
+export default function getSupplier(idSupplier: string) {
+  const { data, error, isLoading } = useSWR(
+    `${endPoint}/suppliers/${idSupplier}`,
+    fetcher
+  );
+
+  return {
+    data: data as Supplier,
+    isLoading,
+    isError: error,
+  };
+}
