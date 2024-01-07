79682e76bfa587318cf5625e3972ede712f69fec
kimthu09 <21521495@gm.uit.edu.vn>
2024-01-07 23:36:06 +0700

    dev: category fe

--- frontend/components/product-manage/category-table.tsx
+++ frontend/components/product-manage/category-table.tsx
@@ -26,33 +26,20 @@ import {
   TableRow,
 } from "@/components/ui/table";
 import { Category } from "@/types";
-import { categories } from "@/constants";
 
 import { useState } from "react";
 import { Input } from "../ui/input";
-
-const data: Category[] = categories;
+import getAllCategory from "@/lib/category/getAllCategory";
+import Loading from "../loading";
+import Paging from "../paging";
+import { useRouter } from "next/navigation";
+import { FaPen } from "react-icons/fa";
+import EditCategory from "./edit-category";
+import { useSWRConfig } from "swr";
+import { endPoint } from "@/constants";
+import { includesRoles } from "@/lib/utils";
 
 export const columns: ColumnDef<Category>[] = [
-  {
-    id: "select",
-    header: ({ table }) => (
-      <Checkbox
-        checked={table.getIsAllPageRowsSelected()}
-        onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
-        aria-label="Select all"
-      />
-    ),
-    cell: ({ row }) => (
-      <Checkbox
-        checked={row.getIsSelected()}
-        onCheckedChange={(value) => row.toggleSelected(!!value)}
-        aria-label="Select row"
-      />
-    ),
-    enableSorting: false,
-    enableHiding: false,
-  },
   {
     accessorKey: "name",
     header: ({ column }) => {
@@ -62,7 +49,7 @@ export const columns: ColumnDef<Category>[] = [
           variant="ghost"
           onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
         >
-          <span className="font-semibold">Danh mục</span>
+          <span className="font-semibold">Thể loại</span>
 
           <CaretSortIcon className="ml-2 h-4 w-4" />
         </Button>
@@ -73,33 +60,42 @@ export const columns: ColumnDef<Category>[] = [
     ),
   },
   {
-    accessorKey: "quantity",
-    header: ({ column }) => (
-      <div className="flex justify-end">
-        <Button
-          variant={"ghost"}
-          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
-        >
-          <span className="font-semibold">Số lượng sản phẩm</span>
-
-          <CaretSortIcon className="ml-2 h-4 w-4" />
-        </Button>
-      </div>
-    ),
+    accessorKey: "actions",
+    header: () => {
+      return <div className="font-semibold flex justify-end">Thao tác</div>;
+    },
     cell: ({ row }) => {
-      return (
-        <div className="text-right font-medium pr-8">
-          {row.getValue("quantity")}
-        </div>
-      );
+      return <></>;
     },
   },
 ];
-export function CategoryTable() {
+export function CategoryTable({
+  searchParams,
+  currentUser,
+}: {
+  searchParams: { [key: string]: string | string[] | undefined };
+  currentUser:
+    | {
+        name?: string | null | undefined;
+        email?: string | null | undefined;
+        image?: string | null | undefined;
+      }
+    | undefined;
+}) {
+  const router = useRouter();
+  const page = searchParams["page"] ?? "1";
+  const { categories, isLoading, isError } = getAllCategory({
+    page: page.toString(),
+  });
+  const data = categories?.data;
+  const totalPage = Math.ceil(
+    categories?.paging.total / categories?.paging.limit
+  );
   const [sorting, setSorting] = useState<SortingState>([]);
   const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([]);
   const [columnVisibility, setColumnVisibility] = useState<VisibilityState>({});
   const [rowSelection, setRowSelection] = useState({});
+
   const table = useReactTable({
     data,
     columns,
@@ -118,92 +114,129 @@ export function CategoryTable() {
       rowSelection,
     },
   });
-
-  return (
-    <div className="w-full">
-      <div className="flex items-center py-4 gap-2">
-        <Input
-          placeholder="Tìm kiếm danh mục"
-          value={(table.getColumn("name")?.getFilterValue() as string) ?? ""}
-          onChange={(event) =>
-            table.getColumn("name")?.setFilterValue(event.target.value)
-          }
-        />
-      </div>
-      <div className="rounded-md border">
-        <Table>
-          <TableHeader>
-            {table.getHeaderGroups().map((headerGroup) => (
-              <TableRow key={headerGroup.id}>
-                {headerGroup.headers.map((header) => {
-                  return (
-                    <TableHead key={header.id}>
-                      {header.isPlaceholder
-                        ? null
-                        : flexRender(
-                            header.column.columnDef.header,
-                            header.getContext()
-                          )}
-                    </TableHead>
-                  );
-                })}
-              </TableRow>
-            ))}
-          </TableHeader>
-          <TableBody>
-            {table.getRowModel().rows?.length ? (
-              table.getRowModel().rows.map((row) => (
-                <TableRow
-                  key={row.id}
-                  data-state={row.getIsSelected() && "selected"}
-                >
-                  {row.getVisibleCells().map((cell) => (
-                    <TableCell key={cell.id}>
-                      {flexRender(
-                        cell.column.columnDef.cell,
-                        cell.getContext()
-                      )}
-                    </TableCell>
-                  ))}
+  const { mutate } = useSWRConfig();
+  const handleCategoryEdited = (name: string) => {
+    mutate(`${endPoint}/categories?page=${page ?? 1}&limit=10`);
+  };
+  if (isError) return <div>Failed to load</div>;
+  if (isLoading) {
+    return <Loading />;
+  } else
+    return (
+      <div className="w-full">
+        <div className="flex items-center py-4 gap-2">
+          <Input
+            placeholder="Tìm kiếm thể loại"
+            value={(table.getColumn("name")?.getFilterValue() as string) ?? ""}
+            onChange={(event) =>
+              table.getColumn("name")?.setFilterValue(event.target.value)
+            }
+          />
+        </div>
+        <div className="rounded-md border">
+          <Table>
+            <TableHeader>
+              {table.getHeaderGroups().map((headerGroup) => (
+                <TableRow key={headerGroup.id}>
+                  {headerGroup.headers.map((header) => {
+                    if (
+                      header.id.includes("actions") &&
+                      (currentUser ||
+                        (currentUser &&
+                          !includesRoles({
+                            currentUser: currentUser,
+                            allowedFeatures: ["CAT_UP_INFO"],
+                          })))
+                    ) {
+                      return null;
+                    } else
+                      return (
+                        <TableHead key={header.id}>
+                          {header.isPlaceholder
+                            ? null
+                            : flexRender(
+                                header.column.columnDef.header,
+                                header.getContext()
+                              )}
+                        </TableHead>
+                      );
+                  })}
                 </TableRow>
-              ))
-            ) : (
-              <TableRow>
-                <TableCell
-                  colSpan={columns.length}
-                  className="h-24 text-center"
-                >
-                  Không tìm thấy bản ghi
-                </TableCell>
-              </TableRow>
-            )}
-          </TableBody>
-        </Table>
-      </div>
-      <div className="flex items-center justify-end space-x-2 py-4">
-        <div className="flex-1 text-sm text-muted-foreground">
-          {table.getFilteredSelectedRowModel().rows.length} of{" "}
-          {table.getFilteredRowModel().rows.length} row(s) selected.
+              ))}
+            </TableHeader>
+            <TableBody>
+              {table.getRowModel().rows?.length ? (
+                table.getRowModel().rows.map((row) => (
+                  <TableRow
+                    key={row.id}
+                    data-state={row.getIsSelected() && "selected"}
+                  >
+                    {row.getVisibleCells().map((cell) => (
+                      <TableCell key={cell.id}>
+                        {cell.id.includes("actions") ? (
+                          currentUser &&
+                          includesRoles({
+                            currentUser: currentUser,
+                            allowedFeatures: ["CAT_UP_INFO"],
+                          }) ? (
+                            <div className=" flex justify-end ">
+                              <EditCategory
+                                category={row.original}
+                                handleCategoryEdited={handleCategoryEdited}
+                              >
+                                <Button
+                                  size={"icon"}
+                                  variant={"ghost"}
+                                  className="rounded-full bg-orange-200/60 hover:bg-orange-200/90 text-primary hover:text-primary"
+                                >
+                                  <FaPen />
+                                </Button>
+                              </EditCategory>
+                            </div>
+                          ) : null
+                        ) : (
+                          flexRender(
+                            cell.column.columnDef.cell,
+                            cell.getContext()
+                          )
+                        )}
+                      </TableCell>
+                    ))}
+                  </TableRow>
+                ))
+              ) : (
+                <TableRow>
+                  <TableCell
+                    colSpan={columns.length}
+                    className="h-24 text-center"
+                  >
+                    Không tìm thấy kết quả.
+                  </TableCell>
+                </TableRow>
+              )}
+            </TableBody>
+          </Table>
         </div>
-        <div className="space-x-2">
-          <Button
-            variant="outline"
-            size="sm"
-            onClick={() => table.previousPage()}
-            disabled={!table.getCanPreviousPage()}
-          >
-            Previous
-          </Button>
-          <Button
-            variant="outline"
-            size="sm"
-            onClick={() => table.nextPage()}
-            disabled={!table.getCanNextPage()}
-          >
-            Next
-          </Button>
+        <div className="flex items-center justify-end space-x-2 py-4">
+          <div className="flex-1 text-sm text-muted-foreground"></div>
+          <Paging
+            page={page.toString()}
+            totalPage={totalPage}
+            onNavigateBack={() =>
+              router.push(`/product/categories?page=${Number(page) - 1}`)
+            }
+            onNavigateNext={() =>
+              router.push(`/product/categories?page=${Number(page) + 1}`)
+            }
+            onPageSelect={(selectedPage) =>
+              router.push(`/product/categories?page=${selectedPage}`)
+            }
+            onNavigateLast={() =>
+              router.push(`/product/categories?page=${totalPage}`)
+            }
+            onNavigateFirst={() => router.push(`/product/categories?page=${1}`)}
+          />
         </div>
       </div>
-    </div>
-  );
+    );
 }
